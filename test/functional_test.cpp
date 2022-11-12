5c58caa6f61e8edc58bbaa38825ea421d69baed5
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-11-12 17:49:45 +0800

    Finalize the implementation for `invoke`.

---
+++ test/functional_test.cpp
@@ -0,0 +1,57 @@
+#include <ds_test.h>
+
+#include <ds/functional.hpp>
+
+using namespace ds;
+
+TEST_SUITE_BEGIN("functional");
+
+TEST_CASE("invoke")
+{
+  {
+    auto f = []() { return 1; };
+    CHECK_EQ(invoke(f), 1);
+  }
+  {
+    auto f = [](int i) { return i; };
+    CHECK_EQ(invoke(f, 1), 1);
+  }
+  {
+    struct s {
+      int f(int i)
+      {
+        return i;
+      }
+    };
+    s obj;
+    CHECK_EQ(invoke(&s::f, obj, 1), 1);
+  }
+  {
+    struct s {
+      int f(int i)
+      {
+        return i;
+      }
+    };
+    s obj;
+    s* ptr = &obj;
+    CHECK_EQ(invoke(&s::f, ptr, 1), 1);
+  }
+  {
+    struct s {
+      int f = 1;
+    };
+    s obj;
+    CHECK_EQ(invoke(&s::f, obj), 1);
+  }
+  {
+    struct s {
+      int f = 1;
+    };
+    s obj;
+    s* ptr = &obj;
+    CHECK_EQ(invoke(&s::f, ptr), 1);
+  }
+}
+
+TEST_SUITE_END();
