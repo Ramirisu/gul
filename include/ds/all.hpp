75c676c97ef1200e2b61c0d77d94cfae485c322f
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-11-17 19:58:23 +0800

    Add header `all.hpp`.

---
+++ include/ds/all.hpp
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2022 Ramirisu (labyrinth dot ramirisu at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#pragma once
+
+#include <ds/config.hpp>
+
+#include <ds/byte.hpp>
+#include <ds/expected.hpp>
+#include <ds/optional.hpp>
+#include <ds/span.hpp>
+#include <ds/string_view.hpp>
+
+#include <ds/fifo_map.hpp>
+#include <ds/lru_map.hpp>
