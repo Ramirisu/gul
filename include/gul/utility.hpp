7cf8975d641408420cdd67031606f8247a5ea30b
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-11-19 20:08:43 +0800

    Implement 'exchange`.

--- include/gul/utility.hpp
+++ include/gul/utility.hpp
@@ -9,6 +9,8 @@
 
 #include <gul/config.hpp>
 
+#include <utility>
+
 GUL_NAMESPACE_BEGIN
 
 struct in_place_t {
@@ -16,5 +18,15 @@ struct in_place_t {
 };
 
 GUL_CXX17_INLINE constexpr in_place_t in_place {};
+
+template <class T, class U = T>
+GUL_CXX14_CONSTEXPR T exchange(T& obj, U&& new_value) noexcept(
+    std::is_nothrow_move_constructible<T>::value&&
+        std::is_nothrow_assignable<T&, U>::value)
+{
+  T old_value = std::move(obj);
+  obj = std::forward<U>(new_value);
+  return std::move(old_value);
+}
 
 GUL_NAMESPACE_END
---
+++ test/utility_test.cpp
@@ -0,0 +1,68 @@
+//
+// Copyright (c) 2022 Ramirisu (labyrinth dot ramirisu at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <gul_test.h>
+
+#include <gul/utility.hpp>
+
+using namespace gul;
+
+TEST_SUITE_BEGIN("utility");
+
+namespace {
+template <bool IsNothrowMoveConstructible, bool IsNothrowMoveAssignable>
+struct movable {
+  movable(int x) noexcept
+      : x(x)
+  {
+  }
+  movable(movable&& other) noexcept(IsNothrowMoveConstructible)
+      : x(other.x)
+  {
+  }
+  movable& operator=(movable&& other) noexcept(IsNothrowMoveAssignable)
+  {
+    x = other.x;
+    return *this;
+  }
+  int x;
+  friend bool operator==(const movable& lhs, const movable& rhs) noexcept
+  {
+    return lhs.x == rhs.x;
+  }
+};
+}
+
+TEST_CASE("exchange")
+{
+  {
+    auto x = movable<true, true>(0);
+    static_assert(noexcept(exchange(x, movable<true, true>(1))), "");
+    CHECK_EQ(exchange(x, movable<true, true>(1)), movable<true, true>(0));
+    CHECK_EQ(x, movable<true, true>(1));
+  }
+  {
+    auto x = movable<false, true>(0);
+    static_assert(!noexcept(exchange(x, movable<false, true>(1))), "");
+    CHECK_EQ(exchange(x, movable<false, true>(1)), movable<false, true>(0));
+    CHECK_EQ(x, movable<false, true>(1));
+  }
+  {
+    auto x = movable<true, false>(0);
+    static_assert(!noexcept(exchange(x, movable<true, false>(1))), "");
+    CHECK_EQ(exchange(x, movable<true, false>(1)), movable<true, false>(0));
+    CHECK_EQ(x, movable<true, false>(1));
+  }
+  {
+    auto x = movable<false, false>(0);
+    static_assert(!noexcept(exchange(x, movable<false, false>(1))), "");
+    CHECK_EQ(exchange(x, movable<false, false>(1)), movable<false, false>(0));
+    CHECK_EQ(x, movable<false, false>(1));
+  }
+}
+
+TEST_SUITE_END();
